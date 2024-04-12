5b27aa4e02732cf9b539237d2711a94877e957f0
GenerousSKA <generousska@gmail.com>
2024-04-11 20:29:58 -0700

    initiate activities

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //alert("Hello World");
 
 let personName = "Polina";
-let personAge = 18;
-const eyecolor = "blue";
-let employed = true;
+let personAge = "18";
+const eyeColor = "blue";
+let isEmployed = true;
+
+console.log("A random person is: " + personName);
+
+let myBed = {
+  brand: "VITAFOAM",
+  color: "gray",
+  isUsed: true,
+  numberOfLegs: 6,
+};
+
+console.log("my table color is:" + myBed.color);
